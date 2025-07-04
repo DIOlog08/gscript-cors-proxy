858e287baf6ca9de56e6347491e2d92b1414caa7
DIOlog08 <e.docs.des@gmail.com>
2025-07-04 21:34:24 +0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
 export default async function handler(req, res) {
-  const url = 'https://script.google.com/macros/s/AKfycbz7GAFvWTME1R1AThpAEHPfN79pq2kReyHpsYfy88RoHEE-7n3bXJd5Z1IKvkiZmpI/exec';
+  const url = 'https://script.google.com/macros/s/AKfycbz7GAFvWTME1RlAThpAEHPFN79pq2kReyHpsYfyB8RoHEE-7n3bjXd5ZlIKvikZmpI/exec';
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') {
-    res.status(200).end();
+    res.status(200).end(); // Για CORS preflight
     return;
   }
 
